81694ba2c4a8288d5231cee2180e06c0f4ba33f6
root <tolentindave@gmail.com>
2025-09-07 07:20:25 +0800

    Change URL

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: false,
-  PATIENT_RECORD_API_URL: 'http://localhost:8081/api/v1/patient-records',
-  PATIENT_CONSULTATION_API_URL: 'http://localhost:8082/api/v1/patient-consultation',
-  PATIENT_DIAGNOSIS_API_URL: 'http://localhost:8083/api/v1/patient-diagnosis',
-  BRANDS_API_URL: 'http://localhost:8084/api/v1/brands',
-  POS_API_URL: 'http://localhost:8085/api/v1/pos',
-  STOCK_ADJUSTMENT_API_URL: 'http://localhost:8086/api/v1/stock-adjustment',
-  MEDICAL_HISTORY_API_URL: 'http://localhost:8087/api/v1/medical-history',
-  PDF_API_URL: 'http://localhost:8088/api/v1/pdf'
+  PATIENT_RECORD_API_URL: 'http://192.168.0.150:8081/api/v1/patient-records',
+  PATIENT_CONSULTATION_API_URL: 'http://192.168.0.150:8082/api/v1/patient-consultation',
+  PATIENT_DIAGNOSIS_API_URL: 'http://192.168.0.150:8083/api/v1/patient-diagnosis',
+  BRANDS_API_URL: 'http://192.168.0.150:8084/api/v1/brands',
+  POS_API_URL: 'http://192.168.0.150:8085/api/v1/pos',
+  STOCK_ADJUSTMENT_API_URL: 'http://192.168.0.150:8086/api/v1/stock-adjustment',
+  MEDICAL_HISTORY_API_URL: 'http://192.168.0.150:8087/api/v1/medical-history',
+  PDF_API_URL: 'http://192.168.0.150:8088/api/v1/pdf'
 };
