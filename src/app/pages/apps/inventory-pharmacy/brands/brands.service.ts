1968cdab44797e0a4bcd7fa5e9bfff3232e27f2a
cittopsagain <cittopsagain663@gmail.com>
2025-12-27 09:02:15 +0800

    feat: Add Edit and View Tab - Add functionality for the Edit and View - Add Delete functionality

--- src/app/pages/apps/inventory-pharmacy/brands/brands.service.ts
+++ src/app/pages/apps/inventory-pharmacy/brands/brands.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {environment} from "../../../../../environments/environment";
 import {Brand} from "./models/brand";
+import {BehaviorSubject} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +10,13 @@ import {Brand} from "./models/brand";
 export class BrandsService {
 
   constructor(private httpClient: HttpClient) {}
+
+  private brandRecordTabBehaviorSubject = new BehaviorSubject<any | null>(null);
+  brandRecordTabBehaviorObservable$ = this.brandRecordTabBehaviorSubject.asObservable();
+
+  setTabIndex(index: number) {
+    this.brandRecordTabBehaviorSubject.next(index);
+  }
 
   getBrands(search: any, sort: string, order: string, page: number) {
     if (search.brandName == null || search.brandName === undefined) {
@@ -24,6 +32,16 @@ export class BrandsService {
   saveBrand(brand: Brand) {
     const href = environment.BRANDS_API_URL;
     return this.httpClient.post<any>(href, brand);
+  }
+
+  updateBrand(brand: Brand) {
+    const href = environment.BRANDS_API_URL;
+    return this.httpClient.put<any>(href, brand);
+  }
+
+  deleteBrand(brandId: number) {
+    const href = environment.BRANDS_API_URL;
+    return this.httpClient.delete<any>(`${href}/${brandId}`);
   }
 }
 
