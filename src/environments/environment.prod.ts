9617202122b289b843c9b7645fe04eca39fce056
cittopsagain <cittopsagain663@gmail.com>
2026-03-27 21:38:35 +0800

    Change API URL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,14 +1,14 @@
 export const environment = {
   production: true,
-  PATIENT_RECORD_API_URL: 'http://192.168.0.151/api/patient-record',
-  PATIENT_CONSULTATION_API_URL: 'http://192.168.0.151/api/patient-consultation',
+  PATIENT_RECORD_API_URL: 'http://192.168.0.151/api/v1/patient-records',
+  PATIENT_CONSULTATION_API_URL: 'http://192.168.0.151/api/v1/patient-consultation',
   PATIENT_DIAGNOSIS_API_URL: 'http://192.168.0.151/api/v1/patient-diagnosis',
-  BRANDS_API_URL: 'http://192.168.0.151/api/brands',
-  POS_API_URL: 'http://192.168.0.151/api/pos',
-  STOCK_ADJUSTMENT_API_URL: 'http://192.168.0.151/api/stock-adjustment',
-  MEDICAL_HISTORY_API_URL: 'http://192.168.0.151/api/medical-history',
-  PDF_API_URL: 'http://192.168.0.151/api/pdf',
-  MEDICINE_API_URL: 'http://192.168.0.151/api/medicine',
-  STOCK_RECEIVING_API_URL: 'http://192.168.0.151/api/stock-receiving',
-  POS_PATIENT_RETURN_API_URL: 'http://192.168.0.151/api/patient-medicine-return'
+  BRANDS_API_URL: 'http://192.168.0.151/api/v1/brands',
+  POS_API_URL: 'http://192.168.0.151/api/v1/pos',
+  STOCK_ADJUSTMENT_API_URL: 'http://192.168.0.151/api/v1/stock-adjustment',
+  MEDICAL_HISTORY_API_URL: 'http://192.168.0.151/api/v1/medical-history',
+  PDF_API_URL: 'http://192.168.0.151/api/v1/pdf',
+  MEDICINE_API_URL: 'http://192.168.0.151/api/v1/medicine',
+  STOCK_RECEIVING_API_URL: 'http://192.168.0.151/api/v1/stock-receiving',
+  POS_PATIENT_RETURN_API_URL: 'http://192.168.0.151/api/v1/patient-medicine-return'
 };
